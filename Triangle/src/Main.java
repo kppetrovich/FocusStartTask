f777b2b45ac0b607e2f6074968557af3c718e15e
paulk24 <paulkur24@gmail.com>
2019-10-15 21:20:37 +0300

    Add fileread stuff

--- Triangle/src/Main.java
+++ Triangle/src/Main.java
@@ -1,14 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-        Coordinate first= new Coordinate(-1,-1);
-        Coordinate second=new Coordinate(10, -1);
-        Coordinate third = new Coordinate(-1, 10);
-        Triangle test = new Triangle(first, second, third);
-        System.out.println(test.isCorrectCoords());
-        test.setSides();
-        System.out.println(test.isTriangleExists());
-        System.out.println(test.isTriangeIsosceles());
-        test.setS();
-        System.out.println(test.getS());
+        String fileIn = args[0];
+        String line = "";
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(fileIn))) {
+            while ((line = br.readLine()) != null) {
+                String lines[] = line.split(" ");
+                Coordinate currentA= new Coordinate(Integer.parseInt(lines[0]), Integer.parseInt(lines[1]));
+                Coordinate currentB = new Coordinate(Integer.parseInt(lines[2]), Integer.parseInt(lines[3]));
+                Coordinate currentC = new Coordinate(Integer.parseInt(lines[4]), Integer.parseInt(lines[5]));
+                Triangle currentTriangle = new Triangle(currentA, currentB, currentC);
+                if(currentTriangle.isCorrectCoords()){
+                    currentTriangle.setSides();
+                    if(currentTriangle.isTriangleExists()){
+                        if(currentTriangle.isTriangeIsosceles()){
+                            currentTriangle.setS();
+                            System.out.println(currentTriangle.getS());
+                        }
+                    }
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
